030af690c2281f233dd1e4cf2317186e1f4f235c
ajmackay <mackay_aaron@protonmail.com>
2023-02-24 14:20:38 +1100

    updated shit

--- scripts/analyses.R
+++ scripts/analyses.R
@@ -6,6 +6,34 @@ source("scripts/functions.R")
 
 load("objects/all-objects.RData")
 
-# Mediation analysis
-x <- lm(dd.total ~ ma.ingest, data = dat)
-summary(x)
+# Prep --------------------------------------------------------------------
+duid.vars <- c("duid.att.risk", "duid.att.sanction", "duid.att.peer")
+
+
+#### Correlation Plot ####
+col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
+
+duid.cor <- cor(select(dat, all_of(duid.vars)))
+duid.cor.ma <- cor(select(filter(dat, ma.ingest), all_of(duid.vars)))
+duid.cor.ndu <- cor(select(filter(dat, !ma.ingest), all_of(duid.vars)))
+
+corrplot::corrplot(duid.cor,
+                   tl.col = "black", tl.srt = 45,
+                   col = col(200), addCoef.col = "black", cl.pos = 'n',
+                   type = 'lower')
+
+#### Regression ####
+##### No group #####
+lm.full <- lm(dd.total ~ duid.att.risk + duid.att.sanction + duid.att.peer, data = dat)
+
+##### MA #####
+lm.ma <- lm(dd.total ~ duid.att.risk + duid.att.sanction + duid.att.peer, data = filter(dat, ma.ingest))
+
+##### NDU #####
+lm.ndu <- lm(dd.total ~ duid.att.risk + duid.att.sanction + duid.att.peer, data = filter(dat, !ma.ingest))
+
+# Overall Multicolinearity Diagnostics
+mctest::omcdiag(ma.lm)
+
+# Individual Multicolinearity Diagnostics
+mctest::imcdiag(ma.lm)
